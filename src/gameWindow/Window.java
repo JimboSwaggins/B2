d919166cfc017a7ed8f4a7142c23d1da57c33f11
NahteKavlozs <NahteKavlozs@gmail.com>
2017-10-02 13:50:23 -0700

    memes

--- src/gameWindow/Window.java
+++ src/gameWindow/Window.java
@@ -1,14 +1,19 @@
 package gameWindow;
 
+import java.awt.Canvas;
+
 import javax.swing.JFrame;
 
 public class Window {
 	JFrame mainWindow;
-	
+	Canvas drawBoard;
 	public void createAndShowGUI() {
 		mainWindow = new JFrame();
 		mainWindow.setSize(1280, 720);
 		mainWindow.setVisible(true);
+		
+		drawBoard = new Canvas();
+		
 	}
 	
 	public Window() {
