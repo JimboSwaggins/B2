f67e6fa2b7d1295c8a2df4215b6838d9ece1fba8
NahteKavlozs <NahteKavlozs@gmail.com>
2017-10-02 14:13:40 -0700

    haha gay

--- src/gameWindow/GameWindow.java
+++ src/gameWindow/GameWindow.java
@@ -1,29 +1,40 @@
 package gameWindow;
 
 import java.awt.Canvas;
+import java.awt.Graphics;
 
 import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+import render.gameUpdate;
 
 public class GameWindow {
 	public JFrame mainWindow;
-	public Canvas drawBoard;
+	public static Canvas drawBoard;
+	
 	public void createAndShowGUI() {
 		mainWindow = new JFrame();
 		mainWindow.setSize(1280, 720);
 		mainWindow.setVisible(true);
 		
 		drawBoard = new Canvas();
+		drawBoard.setSize(1280, 720);
+		
 		
 		mainWindow.add(drawBoard);
+		drawBoard.setVisible(true);
+		
+		mainWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		mainWindow.setVisible(true);
 		
 		
 	}
 	
 	public void run() {
-		
 	}
 	
 	public GameWindow() {
 		createAndShowGUI();
+		gameUpdate.update();
 	}
 }
--- src/render/gameUpdate.java
+++ src/render/gameUpdate.java
@@ -1,11 +1,22 @@
 package render;
 
 import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 
 import gameWindow.GameWindow;
 
 public class gameUpdate extends GameWindow{
-	public void update() {
-		drawBoard.setBackground(Color.BLACK);
+	
+	private static BufferedImage image;
+	public static void update() {	
+		image  = new BufferedImage(1280, 720, BufferedImage.TYPE_INT_RGB);
+		
+		g = (Graphics2D) image.getGraphics();
+		g.setColor(Color.BLACK);
+		g.drawRect(0, 0, 1280, 720);
+		drawBoard.paint(g);
+		
+		
 	}
 }
