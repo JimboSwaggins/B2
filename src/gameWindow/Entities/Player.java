c58adbd4c7de90ef99c46425a60f637446391012
NahteKavlozs <NahteKavlozs@gmail.com>
2017-10-04 12:25:01 -0700

    v0.0.1.5?
    - Began construction of the player class.

--- src/gameWindow/Entities/Player.java
+++ src/gameWindow/Entities/Player.java
@@ -2,8 +2,31 @@ package gameWindow.Entities;
 
 public class Player {
 	private int Health;
-	
 	public int getHealth() {return this.Health;}
 	public void setHealth(int toChange) {this.Health = toChange;}
+	
+	private int xLocation;
+	private int xDelta;
+	
+	private int yLocation;
+	private int yDelta;
+	
+	private int score;
+	//score messing things
+	public void scoreArithmetic(int deltaValue) {this.score += deltaValue;
+	//POSSIBILITY OF ADDING MULTIPLIER VALUES?
+		}
+	public int getScore() {return this.score;}
+	
+	private int bombs;
+	public void bombsArithmetic(int deltaValue) {this.bombs += deltaValue;}
+	public int getBombs() {return this.bombs;}
+	
+	private int lives;
+	public void livesArithmetic(int deltaValue) {this.lives += deltaValue;}
+	public int getLives() {return this.lives;}
+	
+	
+	
 	
 }
