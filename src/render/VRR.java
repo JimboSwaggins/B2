a3bf3d495df06c17b3f78b92bb2e3ae18d74730c
gorebradleyi <gorebradleyi@gmail.com>
2017-10-04 12:59:04 -0700

    working ping

--- src/render/VRR.java
+++ src/render/VRR.java
@@ -1,23 +1,22 @@
 package render;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+
 // Variable Refresh rate
 public class VRR {
 	public static long lastFPS;
 	public static long FPSCounter;
 	public static long currSchedule;
-<<<<<<< HEAD
-	public static long[] listOfTimes;
-
+	public static long currentTime;
+	public static Deque<Long> listOfTimes = new ArrayDeque<Long>();
 	
 	public static void ping() {
+		currentTime = (long) System.currentTimeMillis();
+		listOfTimes.push(currentTime);
 		System.out.println(listOfTimes);
 		
-=======
-	public static long ping() {
-		FPSCounter = System.currentTimeMillis();
-		currSchedule = FPSCounter - lastFPS;
-		//DO YOUR DUMB CALCULUS MEME HERE
-		long waitTime = 000;
-		return waitTime;
->>>>>>> c58adbd4c7de90ef99c46425a60f637446391012
+
 	}
 }
