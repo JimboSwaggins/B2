7831cf82ef64cefc7b9863ab4005cd1980b0440a
gorebradleyi <gorebradleyi@gmail.com>
2017-10-06 13:01:24 -0700

    commitment

--- src/gameWindow/Entities/Player.java
+++ src/gameWindow/Entities/Player.java
@@ -2,7 +2,6 @@ package gameWindow.Entities;
 
 public class Player {
 	private int Health;
-	
 	public int getHealth() {return this.Health;}
 	public void setHealth(int toChange) {this.Health = toChange;}
 	
