528eb0c77ceb36bdff2a6de6198a92f708028c0f
Sloop Listman <NahteKavlozs@gmail.com>
2017-10-10 13:23:42 -0700

    FUCK ME SIDEWAYS AND CALL ME JEANETTE

--- src/render/gameRender.java
+++ src/render/gameRender.java
@@ -12,27 +12,23 @@ public class gameRender extends GameWindow{
 	private static Graphics2D g;	
 	private static BufferedImage image;
 	public static void update() {	
-		image  = new BufferedImage(1280, 720, BufferedImage.TYPE_INT_RGB);
-		
-		g = (Graphics2D) image.getGraphics();
-		g.setColor(Color.BLACK);
-		g.drawRect(0, 0, 1280, 720);
-		g.setColor(Color.GREEN);
-		
 	
-		drawBoard.paint(g);
+
+	
 		//DRAW IMAGES OF STUFF HERE
-		VRR.ping();
+		
 	}
 	
 	public static void draw() {
 		Graphics g2  = drawBoard.getGraphics();
+		//g2 Draws the stuff in the game
 		g2.drawImage(image, 0, 0, null);
 		switch(gameCalculate.status) {
 		case MENU:
+			
 			g2.setColor(Color.RED);
-			g2.drawString("EHLS", 50, 400);
-		
+			g2.drawString("EHLS", 50, 650);
+			VRR.ping();
 			break;
 		default:
 		System.out.println("HA");
