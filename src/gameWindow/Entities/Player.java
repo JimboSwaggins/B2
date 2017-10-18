5b961302d8d4fe0e77a6f0093180f2160faa63b5
gorebradleyi <gorebradleyi@gmail.com>
2017-10-18 12:33:50 -0700

    a

--- src/gameWindow/Entities/Player.java
+++ src/gameWindow/Entities/Player.java
@@ -12,7 +12,6 @@ import gameWindow.GameWindow;
 public class Player extends Entity{
 
 	@SuppressWarnings("unused")
-	private double speed;
 	//GOTTA GO FAST
 	
 	public Player(int x, int y, int health, int dx, int dy) {
@@ -36,7 +35,6 @@ public class Player extends Entity{
 	
 	//TODO Add speed. In other words, get the delta working based on how long the player has been moving or something, and also make it so that the player's movement speed is based of of their base speed oar acceleration
 	public void update() {
-		super.update();
 		if(isUp) {this.yLocation -= speed;}
 		if(isDown) {this.yLocation += speed;}
 		if(isRight) {this.xLocation += speed;}
