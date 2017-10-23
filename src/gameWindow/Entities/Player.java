905f1fecfbc98e42744cf712b520b42845f9f677
NahteKavlozs <NahteKavlozs@gmail.com>
2017-10-23 08:46:00 -0700

    focused behavior

--- src/gameWindow/Entities/Player.java
+++ src/gameWindow/Entities/Player.java
@@ -16,62 +16,77 @@ public class Player extends Entity{
 	private long lastFiring;
 
 	private long rSpeed;
-	
+
 	//GOTTA GO FAST
 
-	
+
 	public Player(int x, int y) {
 		super(x, y, 100, 0, 0);
 		GameWindow.objList.add(this);
 		this.acceleration = .01;
 		this.height = 30.0;
 		this.width = 30.0;
-		
+
 		this.hitR = 3;
-		
+
 		this.entityType = eTYPE.CRTL;
 		this.setFocus(false);
 		this.setFiring(false);
-		
+
 		this.rSpeed = 100;
-		
+
 		this.setLives(3);
 		this.Score = 0;;
 	}
-	
+
 	//TODO Add speed. In other words, get the delta working based on how long the player has been moving or something, and also make it so that the player's movement speed is based of of their base speed oar acceleration
 	public void update() {
-		if(isUp) {
-			if(yVelocity > 0) {
-				this.yVelocity -= 2 * acceleration;
+		if(!isFocus) {
+			if(isUp) {
+				if(yVelocity > 0) {
+					this.yVelocity -= 2 * acceleration;
+				}
+				else {
+					this.yVelocity -= acceleration;
+				}
 			}
-			else {
-				this.yVelocity -= acceleration;
-			}
-			}
-		if(isDown) {
-			if(yVelocity < 0) {
-				this.yVelocity *= 0.99;
-			}
+			if(isDown) {
+				if(yVelocity < 0) {
+					this.yVelocity *= 0.99;
+				}
 				this.yVelocity += acceleration;
 			}
-		if(isRight) {
-			if(xVelocity < 0) {
-				this.xVelocity *= 0.99;
-			}
+			if(isRight) {
+				if(xVelocity < 0) {
+					this.xVelocity *= 0.99;
+				}
 				this.xVelocity += acceleration;
 			}
-		if(isLeft) {
-			if(xVelocity > 0) {
-				this.xVelocity *= 0.99;
-			}
+			if(isLeft) {
+				if(xVelocity > 0) {
+					this.xVelocity *= 0.99;
+				}
 				this.xVelocity -= acceleration;
 			}
-		
-		if(! isLeft && ! isRight) {this.xVelocity *= 0.99;}
-		if(! isUp && ! isDown) {this.yVelocity *= 0.99;}
-		
 
+			if(! isLeft && ! isRight) {this.xVelocity *= 0.99;}
+			if(! isUp && ! isDown) {this.yVelocity *= 0.99;}
+			this.xLocation += this.xVelocity;
+			this.yLocation += this.yVelocity;
+		}if(isFocus) {
+			if(isUp) {
+				this.yLocation -= 0.5;
+			}
+			if(isDown) {
+				this.yLocation += 0.5;
+			}
+			if(isRight) {
+				this.xLocation += 0.5;
+			}
+			if(isLeft) {
+				this.xLocation -= 0.5;
+			}
+		}
 
 		if(this.xLocation > 1280) {
 			this.xLocation = 1280;
@@ -82,18 +97,13 @@ public class Player extends Entity{
 		}if(this.yLocation < 0) {
 			this.yLocation = 0;
 		}
-		
-	
-
-		this.xLocation += this.xVelocity;
-		this.yLocation += this.yVelocity;
 
 		if(this.isFiring()&&(System.currentTimeMillis() - this.lastFiring >= this.rSpeed)) {
-			GameWindow.objList.add(new Bullet(this.xLocation - 1, this.yLocation - 1, this.yVelocity , this.xVelocity , 0, Math.toRadians(90), 8, false, Color.BLUE));
+			GameWindow.objList.add(new Bullet(this.xLocation - 1, this.yLocation - 1, -4, this.xVelocity , 0, Math.toRadians(90), 8, false, Color.BLUE));
 			this.lastFiring = System.currentTimeMillis();
 		}
 	}
-	
+
 	public void draw(Graphics g) {
 		BufferedImage img = null;
 		try {
