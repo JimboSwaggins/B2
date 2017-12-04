8ef8b0dbb3dd8647a4612d436b4496f5f064e2ba
Gorebradleyi <gorebradleyi@gmail.com>
2017-12-04 13:33:11 -0800

    improved collision

--- src/gameWindow/Collision.java
+++ src/gameWindow/Collision.java
@@ -6,14 +6,12 @@ public class Collision extends Thread{
 
 	
 	public void run() {
-		for(int i = 0; i < GameWindow.objList.size(); i++) {
-			Entity e = GameWindow.objList.get(i);
-			for(int b = i; b < GameWindow.objList.size(); b++) {
+		for(int i = 0; i < GameWindow.objList.size() - 1; i++) {
+			for(int b = i + 1; b < GameWindow.objList.size(); b++) {
 
-				Entity f = GameWindow.objList.get(b);
-				System.out.println(e + " " + f);
+			 GameWindow.objList.get(b);
+				System.out.println(GameWindow.objList.get(i) + " " + GameWindow.objList.get(b));
 
-				GameWindow.objList.get(b);
 				
 			}
 		}
