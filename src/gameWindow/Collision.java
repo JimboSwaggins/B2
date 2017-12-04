bfee0b168670c24e18188c1195aac9033f0c79ae
Gorebradleyi <gorebradleyi@gmail.com>
2017-12-04 13:38:52 -0800

    thread collision optimization

--- src/gameWindow/Collision.java
+++ src/gameWindow/Collision.java
@@ -1,7 +1,5 @@
 package gameWindow;
 
-import gameWindow.Entities.Entity;
-
 public class Collision extends Thread{
 
 	
@@ -9,7 +7,6 @@ public class Collision extends Thread{
 		for(int i = 0; i < GameWindow.objList.size() - 1; i++) {
 			for(int b = i + 1; b < GameWindow.objList.size(); b++) {
 
-			 GameWindow.objList.get(b);
 				System.out.println(GameWindow.objList.get(i) + " " + GameWindow.objList.get(b));
 
 				
