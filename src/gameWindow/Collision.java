831fe29c062e9644e68270e62f96ca0fa1f1f375
Gorebradleyi <gorebradleyi@gmail.com>
2017-12-12 14:26:24 -0800

    using sudoku method to remove entities

--- src/gameWindow/Collision.java
+++ src/gameWindow/Collision.java
@@ -12,7 +12,6 @@ public class Collision extends Thread{
 
 	public void run() {
 		for(int i = this.start; i < this.end; i++) {
-			if(!GameWindow.notBullets.get(i).isBullet()){
 				for(int b = i + 1; b < this.end; b++) {
 					//System.out.println(GameWindow.objList.get(i) + " " + GameWindow.objList.get(b));
 					if(GameWindow.notBullets.get(i).getDistance(GameWindow.notBullets.get(b)) < GameWindow.notBullets.get(i).getR() + GameWindow.notBullets.get(b).getR()) {
@@ -25,7 +24,6 @@ public class Collision extends Thread{
 						}
 					}
 				}
-			}
 		}
 	}
 }
