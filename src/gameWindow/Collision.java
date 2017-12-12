197fc646a670388ec5d2bd6ebaacef8ab678d635
Gorebradleyi <gorebradleyi@gmail.com>
2017-12-12 14:21:35 -0800

    collision 1 near finshed

--- src/gameWindow/Collision.java
+++ src/gameWindow/Collision.java
@@ -1,7 +1,5 @@
 package gameWindow;
 
-import gameWindow.Entities.Entity;
-
 public class Collision extends Thread{
 	private int start;
 	private int end;
@@ -13,17 +11,21 @@ public class Collision extends Thread{
 	}
 
 	public void run() {
-
-		for(Entity e: GameWindow.notBullets) {
-			if(!e.isBullet()){
-				for(Entity b : GameWindow.bullets) {
+		for(int i = this.start; i < this.end; i++) {
+			if(!GameWindow.notBullets.get(i).isBullet()){
+				for(int b = i + 1; b < this.end; b++) {
 					//System.out.println(GameWindow.objList.get(i) + " " + GameWindow.objList.get(b));
-					if(e.getDistance(b) < e.getR() + b.getR()&&!e.CtrlCheck()) {
-						b.setX(8000);
+					if(GameWindow.notBullets.get(i).getDistance(GameWindow.notBullets.get(b)) < GameWindow.notBullets.get(i).getR() + GameWindow.notBullets.get(b).getR()) {
+						System.out.println( i + " and " + b);
+						if(GameWindow.notBullets.get(i).CtrlCheck()) {
+							GameWindow.notBullets.get(b).setX(8000);
+						}
+						if(GameWindow.notBullets.get(b).CtrlCheck()) {
+							GameWindow.notBullets.get(i).setX(8000);
+						}
 					}
 				}
 			}
 		}
 	}
 }
-
