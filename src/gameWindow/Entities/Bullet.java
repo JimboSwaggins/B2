36dedcded318817f4c033d5264c450d78f4600c3
Sloop Listman <NahteKavlozs@gmail.com>
2017-12-12 10:56:15 -0800

    tiny change

--- src/gameWindow/Entities/Bullet.java
+++ src/gameWindow/Entities/Bullet.java
@@ -7,6 +7,18 @@ import gameWindow.GameWindow;
 
 public class Bullet extends Entity{
 private Color color;
+
+/**
+ * Creates a new bullet entity
+ * @param xLocation The bullet's starting xLocation
+ * @param yLocation The bullet's starting yLocation
+ * @param deltaX The bullet's velocity on the xAxis
+ * @param deltaY The bullet's velocity on the yAxis
+ * @param speed The speed at which the bullet travels
+ * @param size The size of the bullet
+ * @param hostile Whether or not the bullet will hurt the player
+ * @param color The Color of the bullet
+ */
 	public Bullet(double xLocation, double yLocation, double deltaX, double deltaY, double speed, int size, boolean hostile, Color color) {
 		super(xLocation, yLocation, 1, deltaX, deltaY, 5, 5, 5, 5);
 		this.xVelocity =  deltaX;
@@ -23,12 +35,15 @@ private Color color;
 		GameWindow.objList.add(this);
 			
 	}
-	
 	public void draw(Graphics g) {
 		g.setColor(color);
 		g.fillOval((int)xLocation, (int)yLocation, this.size, this.size);
 	}
 	
+	
+	/**
+	 * Updates the position of the bullet based on the current framerate and a base number.
+	 */
 	public void update() {
 		this.xLocation +=  .6 * this.xVelocity * render.VRR.time;
 		this.yLocation +=  .6 * this.yVelocity * render.VRR.time - .5 * this.acceleration * render.VRR.time;
